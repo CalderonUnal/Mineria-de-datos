146a7a589c28d33fb66c4180f633e7d49c315867
CalderonUnal <jcalderongo@unal.edu.co>
2025-01-28 18:23:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-def main(): 
-  st.title("Clasificación de la base de datos Mnist")
+def main():
+  st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
-if __name__ =="__main__":
+if __name__ == "__main__":
   main()
