449372d39158d6c8032bac733b72fcc1e58fb03b
CalderonUnal <jcalderongo@unal.edu.co>
2025-02-01 17:31:57 -0500

    Update Boston-Housing.py

--- Boston-Housing.py
+++ Boston-Housing.py
@@ -5,5 +5,10 @@ import numpy as np
 import gzip
 import pickle 
 from tensorflow.keras.datasets import boston_housing
-
+## Cargar el modelo ## 
+def load_model():
+  filename = "model_trained_regressor.pkl.gz"
+  with gzip.open(filename, 'rb') as f:
+    model = pickle.load(f)
+  return model
 
